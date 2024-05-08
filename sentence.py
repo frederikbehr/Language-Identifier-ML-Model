863070d9f660da7c76779c8caeb8dd704c8cfb56
fred077a <fred18q3@stud.kea.dk>
2024-05-08 17:09:22 +0200

    added option to train on sentences instead of words

---
+++ sentence.py
@@ -0,0 +1,76 @@
+from fs.osfs import OSFS
+
+from language import Language
+
+# Data stored here
+languages = []
+
+
+def process_file(path):
+  with open(path, "r") as f:
+    result = []
+    lines = f.readlines()
+    for line in lines:
+      result.extend(preprocess(line))
+    return result
+
+
+def preprocess(line):
+  # Removing comma, punctuation and separating words from lines
+  words = line.split(".")
+  processed_words = []
+  for word in words:
+    word = word.strip()
+    word = word.lower()
+    word = word.replace(",", "")
+    word = word.replace(".", "")
+    word = word.replace(":", "")
+    word = word.replace(";", "")
+    word = word.replace("_", "")
+    word = word.replace("", "")
+    if word not in processed_words:
+      processed_words.append(word)
+  return processed_words
+
+
+# 44579
+# 43764
+# Check the data
+
+with OSFS('./data') as fs:
+  # Find directories
+  directories = fs.listdir('.')
+  for index, directory in enumerate(directories):
+    # Each directory / data per language
+    # Get words and add to class object
+
+    print(f"Starting {directory} ({index+1} / {len(directories)})")
+    languages.append(Language(directory, []))
+    files = fs.listdir(directory)
+    for i, file in enumerate(files):
+      # Getting data from each file
+      print(f"    {file} ({i+1} / {len(files)})")
+      data = process_file("./data/" + directory + "/" + file)
+      languages[-1].add_words(data)
+
+# Clear dataset.csv, so it won't contain duplicates
+with open("./dataset.csv", 'w', newline='') as file:
+  file.truncate(0)
+
+# Set column labels
+with open("./dataset.csv", 'w') as file:
+  file.write("Word,Language\n")
+
+# Get smallest size
+smallestValue = 100000
+for language in languages:
+  if len(language.words) < smallestValue:
+    smallestValue = len(language.words)
+print(smallestValue)
+
+for language in languages:
+  print(language)
+  print("Writing to csv")
+  language.write_to_csv(smallestValue)
+
+print("Application finished.")
